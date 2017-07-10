deb1f237842115834ffb66d6e3f209f803adbaec
Suhit Kumar <suhit.kmr1@gmail.com>
2017-07-10 12:12:07 +0530

    FreeCode Camp Project-Request Header Parser Microservice

---
+++ README.md
@@ -0,0 +1,16 @@
+#
+
+Created by suhitk1
+
+[Github](https://github.com/suhitk1) | [FreeCodeCamp](http://www.freecodecamp.com/suhitk1) | [CodePen](http://codepen.io/suhitk1/) | [E-Mail](mailto:suhit.kmr1@gmail.com)
+
+# FreeCodeCamp API Basejump: Request Header Parser Microservice
+
+## User stories:
+1. I can get the IP address, language and operating system for my browser.
+
+## Example usage:
+
+```js
+{"ipaddress":"xxx.xxx.xxx.xxx","language":"en-US","software":"X11; Linux x86_64"}
+```
---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var routes = require('./app/routes/index.js');
+    
+var port = process.env.PORT || 8080;        // set our port
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+    
+// The format follows as, alias to use for real path, also allows permission to such path.
+//app.use('/api', express.static(process.cwd() + '/app/api'));
+    
+routes(app);
+
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port);
+});
